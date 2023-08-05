9320046f2a79bbf7c8b18f6a54228d4e2aa75e0d
maesterfox <talentedmrfox@hotmail.com>
2023-08-05 19:23:05 +0100

    updates to table format and js code

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-// Define variables
 let matches = [];
 let variations = 0;
 let combinations = 0;
@@ -14,28 +13,37 @@ function startPredictions() {
         return;
     }
 
-    // Prompt for team and strength for each match
+    // Prompt for team, strength, and average goals for each match
     for (let i = 0; i < numberOfMatches; i++) {
         const homeTeam = prompt(`Enter team ${i + 1} (Home team):`);
         const homeTeamStrength = parseInt(prompt(`Team ${homeTeam} strength (1-100):`));
+        const homeTeamAvgGoalsScored = parseFloat(prompt(`Average goals scored per game by ${homeTeam}:`));
+        const homeTeamAvgGoalsConceded = parseFloat(prompt(`Average goals conceded per game by ${homeTeam}:`));
+
         const awayTeam = prompt(`Enter team ${i + 1} (Away team):`);
         const awayTeamStrength = parseInt(prompt(`Team ${awayTeam} strength (1-100):`));
+        const awayTeamAvgGoalsScored = parseFloat(prompt(`Average goals scored per game by ${awayTeam}:`));
+        const awayTeamAvgGoalsConceded = parseFloat(prompt(`Average goals conceded per game by ${awayTeam}:`));
 
         if (
-            !homeTeam ||
-            !awayTeam ||
-            isNaN(homeTeamStrength) || homeTeamStrength < 1 || homeTeamStrength > 100 ||
-            isNaN(awayTeamStrength) || awayTeamStrength < 1 || awayTeamStrength > 100
+            !homeTeam || isNaN(homeTeamStrength) || homeTeamStrength < 1 || homeTeamStrength > 100 ||
+            !awayTeam || isNaN(awayTeamStrength) || awayTeamStrength < 1 || awayTeamStrength > 100 ||
+            isNaN(homeTeamAvgGoalsScored) || isNaN(homeTeamAvgGoalsConceded) ||
+            isNaN(awayTeamAvgGoalsScored) || isNaN(awayTeamAvgGoalsConceded)
         ) {
-            alert("Invalid input. Please enter valid team names and strengths (1-100).");
+            alert("Invalid input. Please enter valid team names, strengths (1-100), and average goals.");
             return;
         }
 
         matches.push({
             homeTeam,
             homeTeamStrength,
+            homeTeamAvgGoalsScored,
+            homeTeamAvgGoalsConceded,
             awayTeam,
-            awayTeamStrength
+            awayTeamStrength,
+            awayTeamAvgGoalsScored,
+            awayTeamAvgGoalsConceded
         });
     }
 
@@ -59,11 +67,13 @@ function generateOutcomes() {
     for (let i = 0; i < uniqueVariations.length; i++) {
         const variation = uniqueVariations[i];
         const outcome = getMatchOutcome(variation);
+        const predictedGoals = getPredictedGoals(variation);
         const outcomeRow = `
             <tr>
                 <td>${i + 1}</td>
                 <td>${variation.map(match => `${match.homeTeam} vs ${match.awayTeam}`).join('<br>')}</td>
                 <td>${outcome}</td>
+                <td>${predictedGoals}</td>
             </tr>
         `;
         tableBody.insertAdjacentHTML('beforeend', outcomeRow);
@@ -93,12 +103,29 @@ function getMatchOutcome(matchVariation) {
     let outcome = '';
     for (const match of matchVariation) {
         const totalStrength = match.homeTeamStrength + match.awayTeamStrength;
-        const probHomeTeam = match.homeTeamStrength / totalStrength;
-        const probAwayTeam = match.awayTeamStrength / totalStrength;
+        const homeTeamAdvantage = match.homeTeamStrength * 0.10;
+
+        const homeTeamAdjustedAvgGoalsScored = match.homeTeamAvgGoalsScored + match.homeTeamAvgGoalsConceded;
+        const awayTeamAdjustedAvgGoalsScored = match.awayTeamAvgGoalsScored + match.awayTeamAvgGoalsConceded;
+
+        const probHomeTeam = (match.homeTeamStrength + homeTeamAdvantage + homeTeamAdjustedAvgGoalsScored - match.awayTeamAvgGoalsConceded) / totalStrength;
+        const probAwayTeam = (match.awayTeamStrength + awayTeamAdjustedAvgGoalsScored - match.homeTeamAvgGoalsConceded) / totalStrength;
 
         outcome += `${match.homeTeam}: ${formatPercentage(probHomeTeam)}, ${match.awayTeam}: ${formatPercentage(probAwayTeam)}<br>`;
     }
     return outcome;
+}
+
+function getPredictedGoals(matchVariation) {
+    let predictedGoals = '';
+    for (const match of matchVariation) {
+        const homeTeamAdvantage = match.homeTeamStrength * 0.02;
+        const homeTeamGoals = Math.round((match.homeTeamAvgGoalsScored + homeTeamAdvantage + match.awayTeamAvgGoalsConceded) / 2);
+        const awayTeamGoals = Math.round((match.awayTeamAvgGoalsScored + match.homeTeamAvgGoalsConceded) / 2);
+
+        predictedGoals += `${match.homeTeam} vs ${match.awayTeam}: ${homeTeamGoals} - ${awayTeamGoals}<br>`;
+    }
+    return predictedGoals;
 }
 
 function formatPercentage(value) {
